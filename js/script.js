f5a0b51ea51ff136db24578a6daefc670e648a4d
PolyakovaIra <era_23_12@mail.ru>
2016-12-06 16:49:52 +0200

    js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,49 @@
-var link-button = document.querySelector(".data-button");
+var link = document.querySelector(".data-button");
 var button = document.querySelector(".button");
-var close = document.querySelector(".container");
-var storage = localStorage.getItem ("amount")
+var form = button.querySelector("input-data");
+var data = button.querySelector("[id=form-data]");
+var data2 = button.querySelector("[id=form-data2]");
+var human = button.querySelector("[id=human]");
+var children = button.querySelector("[id=children]");
+var storage = localStorage.getItem("data");
+var storage = localStorage.getItem("data2");
+var storage = localStorage.getItem("human");
 
-link-button.addEventListener("click",function(event) {
+link.addEventListener("click",function(event) {
   event.preventDefault();
   button.classList.add("button-show");
+  data.focus();
+
+  if (storge){
+    data.value = storage;
+    data2.focus();
+  } else {
+    data.focus();
+  }
 });
-close.addEventListener("click", function(event) {
-  event.preventDefault();
-  button.classList.remove("button-show");
-});
-window.addEventListener("keydown", function(event) {
+
+window.addEventListener("keydown",function(event) {
   if (event.keyCode === 27) {
     if (button.classList.contains("button-show")) {
       button.classList.remove("button-show");
     }
   }
-};
+});
+submit.addEventListener("submit", function(event) {
+  if (!data.value || !data2.value) {
+  event.preventDefault();
+  button.classList.add("button-error");
+  console.log("Введите даты заезда и выезда");
+} else {
+  localStorage.setItem("data", data.value);
+  localStorage.setItem("data2", data2.value);
+}
+});
+submit.addEventListener("submit", function(event) {
+  if (!human.value) {
+  event.preventDefault();
+  console.log("Введите количество людей");
+}else {
+  localStorage.setItem("human", human.value);
+}
+});
